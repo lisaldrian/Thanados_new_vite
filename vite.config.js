feb50f82761f158fae34f39d1000f185ac7565ee
lisaldrian <lis.aldrian@gmail.com>
2024-02-29 13:43:00 +0100

    scss_working

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import sass from 'vite-plugin-sass';
 
 export default defineConfig({
-  plugins: [sass()],
+  plugins: [
+    sass({
+      // Specify the output directory for the CSS file
+      output: 'dist/css', // Adjust the directory as per your project structure
+    }),
+  ],
 });
