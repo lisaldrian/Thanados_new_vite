66588729b469b1f33f8d30803fe7959c3c9aa8c9
lisaldrian <lis.aldrian@gmail.com>
2024-03-05 15:24:10 +0100

    home_navbar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import sass from 'vite-plugin-sass';
 
 export default defineConfig({
+    root: resolve(__dirname), // Adjust the path if your vite.config.js is not in the project root
+    resolve: {
+      alias: {
+        '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+      }
+    },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'), // Default entry point
+        anotherPage: resolve(__dirname, 'home/home.html'), // New entry point
+      },
+    },
+  },
   plugins: [
     sass({
       // Specify the output directory for the CSS file
