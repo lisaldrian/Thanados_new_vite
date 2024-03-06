e242a6e01f346a2d1b242a6d296e7dac0ed5d9ea
lisaldrian <lis.aldrian@gmail.com>
2024-03-06 17:03:10 +0100

    background

---
+++ home/js/background.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function() {
+    var container = document.getElementById('background-container');
+    var image = new Image();
+        
+    image.onload = function() {
+        console.log('Image loaded');
+        console.log('Image width:', image.width);
+        console.log('Image height:', image.height);
+
+        var aspectRatio = image.width / image.height;
+        var containerWidth = container.offsetWidth;
+        var containerHeight = containerWidth / aspectRatio;
+            
+        container.style.height = containerHeight + 'px';
+    };
+        
+    image.src = '/images/Blank_map_of_Europe_central_crop.png';
+});
+
