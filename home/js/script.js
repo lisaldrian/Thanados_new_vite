2bc661bd73636b340b84bebf23e3fec70c7b4616
lisaldrian <lis.aldrian@gmail.com>
2024-03-12 10:15:54 +0100

    dark_light_navbar

--- home/js/script.js
+++ home/js/script.js
@@ -1,24 +1,16 @@
-/* 
-The first time the page is loaded, the color mode set on the preference 
-is used and set as 'default' in the local storage. 
-Changing the default preferences works the same way as changing the 
-color mode using the buttons, if the page is loaded.
-When the page is reloaded, whatever is the value set on the local storage
-has precedence over the values in the preference. If the preference
-changed after the page was visited - and the page is not loaded - 
-the last value saved on the local storage is loaded. 
-*/
-
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
+const navbar = document.getElementById('mynavbar');
 
 const setDarkMode = () => {
-  document.querySelector('body').classList = 'dark';
+  document.body.classList = 'dark';
+  navbar.classList = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top';
   localStorage.setItem('colorMode', 'dark');
 };
 
 const setLightMode = () => {
-  document.querySelector('body').classList = 'light';
+  document.body.classList = 'light';
+  navbar.classList = 'navbar navbar-expand-lg navbar-light bg-light fixed-top';
   localStorage.setItem('colorMode', 'light');
 };
 
@@ -29,13 +21,12 @@ const colorModeFromLocalStorage = () => {
 const colorModeFromPreferences = () => {
   return window.matchMedia('(prefers-color-scheme: dark)').matches 
               ? 'dark'
-              : 'light' // If preference is set or does not match anything (light is default)
+              : 'light';
 };
 
 const loadAndUpdateColor = () => {
-  // local storage has precendence over the prefers-color-scheme
   const color = colorModeFromLocalStorage() || colorModeFromPreferences();
-  color == 'dark' ? darkButton.click() : lightButton.click();
+  color === 'dark' ? setDarkMode() : setLightMode();
 };
 
 // when the inputs are clicked, check which radio button is checked and change the color
@@ -52,6 +43,6 @@ window.matchMedia('(prefers-color-scheme: dark)')
       .addEventListener('change', (event) => {
         event.matches ? darkButton.click() : lightButton.click();
       });
-      
+
 // Load the right color on startup - localStorage has precedence
 loadAndUpdateColor();
