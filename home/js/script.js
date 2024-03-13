8ef8fa688614d881027885961cc81b610ea6249b
lisaldrian <lis.aldrian@gmail.com>
2024-03-13 13:06:25 +0100

    dark_light_toggleposition_fix

--- home/js/script.js
+++ home/js/script.js
@@ -26,6 +26,8 @@ const colorModeFromPreferences = () => {
 
 const loadAndUpdateColor = () => {
   const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+  color === 'dark' ? darkButton.checked = true : lightButton.checked = true;
+  // Update the color mode based on the stored or preferred mode
   color === 'dark' ? setDarkMode() : setLightMode();
 };
 
@@ -33,9 +35,16 @@ const loadAndUpdateColor = () => {
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
 radioButtons.forEach(button => {
   button.addEventListener('click', (event) => {
-    darkButton.checked ? setDarkMode() : setLightMode();
+    if (event.target === darkButton) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
   });
 });
+
+// Load the right color on startup - localStorage has precedence
+loadAndUpdateColor();
 
 // when the prefers-color-scheme changes, this event will be emitted
 // event reflects the media query, if it matches, the new color is dark, else it is light
@@ -44,5 +53,4 @@ window.matchMedia('(prefers-color-scheme: dark)')
         event.matches ? darkButton.click() : lightButton.click();
       });
 
-// Load the right color on startup - localStorage has precedence
-loadAndUpdateColor();
+
