80c1464fa4838cf8252961ae6915d8fe1995bda8
lisaldrian <lis.aldrian@gmail.com>
2024-04-10 13:51:26 +0200

    deleted old sliders

--- places/js/rangeSlider.js
+++ places/js/rangeSlider.js
@@ -1,5 +1,5 @@
 let s1vals = [-20, -10, 10, 20];
-let s1animals = ["🐬","🐢","🐷","🦁"];
+let s1animals = ["🐬", "🐢", "🐷", "🦁"];
 let slider = new RangeSliderPips({
   target: document.getElementById("slider"),
   props: {
@@ -19,20 +19,19 @@ let slider = new RangeSliderPips({
 let slider2 = new RangeSliderPips({
   target: document.getElementById("slider2"),
   props: {
-    min: 0,
-    max: 5000,
+    min: -500,
+    max: 1500,
     range: true,
     pushy: true,
-    values: [0, 2000],
+    values: [500, 1000],
     step: 10,
     pips: true,
     pipstep: 10,
     all: "label",
-    prefix: "£"
+    //suffix: " AD", "BC" for values
+    
   }
 });
-
-// *********************************************
 
 let $btn = document.querySelector(".s1btn");
 let $s21 = document.querySelector(".s21");
@@ -41,13 +40,13 @@ let $s22 = document.querySelector(".s22");
 // slider 1 bindings
 
 function setHandles(e) {
-  let labels = e.detail.values;
-  let handles = document.getElementById("slider").querySelectorAll(".rangeHandle");
-  let handle = document.getElementById("slider").querySelectorAll(".rangeHandle.active");
-  Array.prototype.forEach.call(handles,(el,i)=> {
-    el.children[1].innerHTML = labels[i] + s1animals[i];
-  });
-  s1vals = e.detail.values;
+let labels = e.detail.values;
+let handles = document.getElementById("slider").querySelectorAll(".rangeHandle");
+let handle = document.getElementById("slider").querySelectorAll(".rangeHandle.active");
+Array.prototype.forEach.call(handles,(el,i)=> {
+  el.children[1].innerHTML = labels[i] + s1animals[i];
+});
+s1vals = e.detail.values;
 }
 
 setHandles({ detail: { values: s1vals }});
@@ -57,10 +56,10 @@ setHandles({ detail: { values: s1vals }});
 slider.$on('change', setHandles);
 
 $btn.addEventListener( "click", function() {
-  $btn.innerText = "the values are: " + s1vals;
-  setTimeout(() => {
-    $btn.innerText = "Get Values";
-  }, 1500 );
+$btn.innerText = "the values are: " + s1vals;
+setTimeout(() => {
+  $btn.innerText = "Get Values";
+}, 1500 );
 })
 
 // slider 2 bindings 
@@ -68,16 +67,16 @@ $btn.addEventListener( "click", function() {
 // .$on() is a svelte component function
 // https://svelte.dev/docs#$on
 slider2.$on('change', function(e) {
-  $s21.value = e.detail.values[0];
-  $s22.value = e.detail.values[1];
+$s21.value = e.detail.values[0];
+$s22.value = e.detail.values[1];
 });
 
 // .set() is a svelte component function
 // https://svelte.dev/docs#$set
 $s21.addEventListener("change", (e) => {
-  slider2.$set({ values: [ $s21.value, $s22.value ]});
+slider2.$set({ values: [ $s21.value, $s22.value ]});
 });
 
 $s22.addEventListener("change", (e) => {
-  slider2.$set({ values: [ $s21.value, $s22.value ]});
+slider2.$set({ values: [ $s21.value, $s22.value ]});
 });
